646b96b14eb22536625a04260d1792d855c5936b
Mr. Wallace <pwallace@madeira.org>
2020-08-18 10:13:11 -0400

    Labs 1 & 2

---
+++ src/madeira/cs/Lab1.java
@@ -0,0 +1,29 @@
+package madeira.cs;
+
+/**
+ * Author: YOUR_NAME
+ * Date: 1/1/1970
+ **/
+public class Lab1 {
+    public static void main(String[] args) {
+       /*
+         Instructions For Use
+         ------
+         Write a program that does the following
+         I. Declare and instantiate three strings
+            a. Print each string separately
+         II. Re-assign a new value to the strings you declared
+            a. Print the new strings separately
+         III. Visit the link in the README.md section
+            a. Find three string methods you like
+            b. Use them however you so choose
+       */
+
+        // Part I code below
+
+        // Part II code below
+
+        // Part III code below
+
+    }
+}
---
+++ src/madeira/cs/Lab2.java
@@ -0,0 +1,24 @@
+package madeira.cs;
+
+/**
+ * Author: YOUR_NAME
+ * Date: 1/1/1970
+ **/
+public class Lab2 {
+    public static void main(String[] args) {
+        /*
+            Instructions for Use
+            ----
+            Write a program that does the following
+                I. Read your name
+                    a. Print a greeting
+                II. Read your favorite artist or author
+                    a. Print your favorite song or book by that artist
+                III. Read your age
+                    a. Print the number of days you've been alive (an estimate is fine)
+         */
+        // Part I code below
+        // Part II code below
+        // Part III code below
+    }
+}
