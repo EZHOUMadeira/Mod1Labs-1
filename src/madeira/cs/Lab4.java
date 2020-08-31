da759c5ce9f14f6fbe9a110c6ed3b3ad9c13bfa3
Mr. Wallace <pwallace@madeira.org>
2020-08-31 16:52:00 -0400

    updating lab4

--- src/madeira/cs/Lab4.java
+++ src/madeira/cs/Lab4.java
@@ -1,4 +1,19 @@
 package madeira.cs;
 
 public class Lab4 {
+	/*
+		Instructions for use
+		-------
+		Write a program that does the following
+		I. Creates a new class named Artist.
+			a. Artist should have 3 member variables
+				(a)i. name
+				(a)ii. album
+				(a)iii. twitterHandle
+		II. Reads the user's favorite artist
+		II. Opens the file artistList.txt
+		III. Reads the list of artists in the above file & prints:
+			a. Match if a match between the user's artist and textfile is found.
+
+	*/
 }
