9f9b322ab212978ea8479f5478aa6dea2921fa47
Mr. Wallace <pwallace@madeira.org>
2020-08-31 16:27:26 -0400

    updating lab3

--- src/madeira/cs/Lab3.java
+++ src/madeira/cs/Lab3.java
@@ -2,6 +2,26 @@ package madeira.cs;
 
 public class Lab3 {
     public static void main(String[] args) {
+	 /*
+           Instructions For Use
+           ------
+           Write a program that does the following
+           I. Reads the user's age and prints if they're permitted to:
+              a. Drive
+	      b. Get a tattoo
+              c. Run for President of the United States
+           II. Reads the user's birthday month and prints:
+              a. Their astrological sign
+	      b. The season of said month
+              c. The number of days in the month
+           III. Prints the estimated value of their age in days.
+       */
 
-    }
+        // Part I code below
+
+        // Part II code below
+
+        // Part III code below
+
+    }	
 }
