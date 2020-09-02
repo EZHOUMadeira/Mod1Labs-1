ef6b83013d818ae534c50286810fcccfec376750
Mr. Wallace <67603153+mrWallaceMadeira@users.noreply.github.com>
2020-09-02 09:57:58 -0400

    Update Lab5.java

--- src/madeira/cs/Lab5.java
+++ src/madeira/cs/Lab5.java
@@ -1,4 +1,9 @@
 package madeira.cs;
 
 public class Lab5 {
+  	II. Reads the user's favorite artist
+		III. Opens the file artistList.txt
+		IV. Creates a method named artistMatch which does the following:
+			a. Reads the list of artists in the above file
+			b. Prints match if a match between the user's artist and textfile is found.
 }
